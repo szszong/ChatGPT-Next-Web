0343ac1cb33f6a7bc4c77ee953eba81f57ec99a2
szszong <135265231+szszong@users.noreply.github.com>
2023-06-02 14:11:31 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>最先进的直播交互数字人系统</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          .
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
